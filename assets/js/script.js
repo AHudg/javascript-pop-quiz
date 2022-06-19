c755b6c7eff54db3e679699df0c3b5704dc08ad4
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-06-19 15:37:47 -0500

    Created functions createQuiz, updateTimer, and nextQuestion to iterate through the quiz pages and respond by updating the timer appropriately

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,39 @@
+var timeLeft = 75;
 // creates our DOM element for the score (timer)
 var scoreEl = document.getElementById("score");
 // creates our DOM element for the button to start the quiz
 var beginBtnEl = document.querySelector("button[name='begin']");
 // creates our DOM element for the <div> containing the quiz questions/answers
 var quizEl = document.querySelector("div[class='focal-pt-bg']");
+// creates our DOM element for the <ol> that will contain the answer choices
+var orderEl = document.querySelector("ol[class='answer-list']");
+console.log(orderEl)
 // creates our DOM element for the <p> that is the quiz question
-var quizQuestion = document.getElementById("quiz-questions");
+var quizQuestionEl = document.getElementById("quiz-questions");
+// creates list items to later append to the quiz
+var listItemOneEl = document.createElement('li');
+var listItemTwoEl = document.createElement('li');
+var listItemThreeEl = document.createElement('li');
+var listItemFourEl = document.createElement('li');
 
+// initialize an array to be used like a for loops but waits to iterate only after a <li> is clicked
+var arrayIndex = 0;
+
+// creates the array containing each question to be iterated through
 var questionArray = ["Which of the following is NOT a commonly used data type?","The condition in an if/else statement is enclosed with ___?","Arrays in JavaScript can be used to store which of the following data types?","String values must be enclosed within ___ when being assigned to variables?","A very useful tool used during development and debugging for printing content to the debugger is?"];
-    
+
+// creates five objects that contain the four answer choices
 ansObjOne = {
     ansOne: "Strings",
     ansTwo: "Booleans",
-    ansNestObj: {
-        ansThree: "Alerts",
-        corrent: true
-    },
+    ansThree: "Alerts",
     ansFour: "Numbers"
 };
 
 ansObjTwo = {
     ansOne: "Quotes",
     ansTwo: "Curly brackets",
-    ansNestObj: {
-        ansThree: "Parenthesis",
-        correct: true
-    },
+    ansThree: "Parenthesis",
     ansFour: "Square brackets"
 };
 
@@ -33,18 +41,12 @@ ansObjThree = {
     ansOne: "Numbers and strings",
     ansTwo: "Other arrays",
     ansThree: "Booleans",
-    ansNestObj: {
-        ansFour: "All of the above",
-        corect: true
-    }
+    ansFour: "All of the above",
 };
 
 ansObjFour = {
     ansOne: "Commas",
-    ansNestObj: {
-        ansTwo: "Curly brackets",
-        correct: true
-    },
+    ansTwo: "Curly brackets",
     ansThree: "Quotes",
     ansFour: "Parenthesis"
 };
@@ -53,15 +55,14 @@ ansObjFive = {
     ansOne: "JavaScript",
     ansTwo: "Terminal/Git Bash",
     ansThree: "For loops",
-    ansNestObj: {
-        ansFour: "Console.log",
-        correct: true
-    }
+    ansFour: "Console.log",
 };
 
+// places those objects in an array at the same index as their corresponding question
 var answersArray = [ansObjOne, ansObjTwo, ansObjThree, ansObjFour, ansObjFive];
 
-var correctArray = ["Alerts","Parenthesis","All of the above","Curly brackets","Console.log"];
+// creates an array with the correct string answer
+var correctArray = ["Alerts","Parenthesis","All of the above","Quotes","Console.log"];
 
 var startQuiz = function() {
     // calls the timer to start counting down (keeping score)
@@ -71,20 +72,100 @@ var startQuiz = function() {
     beginBtnEl.setAttribute('style','display: none');
     // styles the <div> containing the quiz questions/answers
     quizEl.setAttribute('style','border: #DB7093 5px solid; background:#FFE4E1;');
-    // styles the question
-    quizQuestion.setAttribute('style','color: var(--secondary); font-weight:bold');
 
-    // createQuestions();
+    // styles the question
+    quizQuestionEl.setAttribute('style','color: var(--secondary); font-weight:bold; padding:5px');
+
+    // append the list items that were populated at initialization to the <ol>
+    orderEl.appendChild(listItemOneEl);
+    orderEl.appendChild(listItemTwoEl);
+    orderEl.appendChild(listItemThreeEl);
+    orderEl.appendChild(listItemFourEl);
+
+    // call createQuiz() to populate the first page of the quiz and style the elements
+    createQuiz();
+
+    // when one of the list items are selected, call functiong nextQuestion()
+    orderEl.addEventListener("click", updateTimer);
 };
 
-var createQuestions = function() {
+var updateTimer = function(event) {
+    var element = event.target;
+    console.log(element);
+    console.dir(element);
+    if (element.textContent === correctArray[arrayIndex]) {
+        timeLeft += 5;
+    } else {
+        timeLeft -= 10;
+    }
+    return nextQuestion();
+};
 
-}
+// function changes the text to iterate through the questions/answers and styles accordingly
+var createQuiz = function() {
+    quizQuestionEl.textContent = questionArray[arrayIndex];
+    listItemOneEl.textContent = answersArray[arrayIndex].ansOne;
+    listItemTwoEl.textContent = answersArray[arrayIndex].ansTwo;
+    listItemThreeEl.textContent = answersArray[arrayIndex].ansThree;
+    listItemFourEl.textContent = answersArray[arrayIndex].ansFour;
+
+    listItemOneEl.className = "answers";
+    listItemTwoEl.className = "answers";
+    listItemThreeEl.className = "answers";
+    listItemFourEl.className = "answers";
+
+    listItemOneEl.addEventListener("mouseover",function(event) {
+        listItemOneEl.className = "answers-hover";
+    });
+
+    listItemOneEl.addEventListener("mouseout",function(event) {
+        listItemOneEl.className = "answers";
+    });
+
+    listItemTwoEl.addEventListener("mouseover",function(event) {
+        listItemTwoEl.className = "answers-hover";
+    });
+
+    listItemTwoEl.addEventListener("mouseout",function(event) {
+        listItemTwoEl.className = "answers";
+    });
+
+    listItemThreeEl.addEventListener("mouseover",function(event) {
+        listItemThreeEl.className = "answers-hover";
+    });
+
+    listItemThreeEl.addEventListener("mouseout",function(event) {
+        listItemThreeEl.className = "answers";
+    });
+
+    listItemFourEl.addEventListener("mouseover",function(event) {
+        listItemFourEl.className = "answers-hover";
+    });
+
+    listItemFourEl.addEventListener("mouseout",function(event) {
+        listItemFourEl.className = "answers";
+    });
+
+    // return arrayIndex++;
+};
+
+// function to add/subtract timer/score and proceed to the next question
+var nextQuestion = function() {
+    arrayIndex++;
+    quizQuestionEl.textContent = questionArray[arrayIndex];
+    listItemOneEl.textContent = answersArray[arrayIndex].ansOne;
+    listItemTwoEl.textContent = answersArray[arrayIndex].ansTwo;
+    listItemThreeEl.textContent = answersArray[arrayIndex].ansThree;
+    listItemFourEl.textContent = answersArray[arrayIndex].ansFour;
+
+    listItemOneEl.className = "answers";
+    listItemTwoEl.className = "answers";
+    listItemThreeEl.className = "answers";
+    listItemFourEl.className = "answers";
+};
 
 // creates the function that will house the score (timer) and count down
 var countdown = function() {
-    var timeLeft = 75;
-
     // use the setInterval to call this function every 1000 ms
     var firstInterval = setInterval(function() {
         // as long as time is left...
