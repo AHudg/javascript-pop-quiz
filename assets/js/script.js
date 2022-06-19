2082ea6b2e2b1078179c62f699314c1470e053ec
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-06-19 16:15:02 -0500

    Created an endQuiz() function to end after the questionArray is finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
+// Initializes the timer/score
 var timeLeft = 75;
 // creates our DOM element for the score (timer)
 var scoreEl = document.getElementById("score");
-// creates our DOM element for the button to start the quiz
-var beginBtnEl = document.querySelector("button[name='begin']");
+
 // creates our DOM element for the <div> containing the quiz questions/answers
 var quizEl = document.querySelector("div[class='focal-pt-bg']");
-// creates our DOM element for the <ol> that will contain the answer choices
-var orderEl = document.querySelector("ol[class='answer-list']");
-console.log(orderEl)
+// creates our DOM element for the button to start the quiz
+var beginBtnEl = document.querySelector("button[name='begin']");
+
+
 // creates our DOM element for the <p> that is the quiz question
 var quizQuestionEl = document.getElementById("quiz-questions");
+
+// creates our DOM element for the <ol> that will contain the answer choices
+var orderEl = document.querySelector("ol[class='answer-list']");
 // creates list items to later append to the quiz
 var listItemOneEl = document.createElement('li');
 var listItemTwoEl = document.createElement('li');
@@ -18,51 +22,6 @@ var listItemFourEl = document.createElement('li');
 
 // initialize an array to be used like a for loops but waits to iterate only after a <li> is clicked
 var arrayIndex = 0;
-
-// creates the array containing each question to be iterated through
-var questionArray = ["Which of the following is NOT a commonly used data type?","The condition in an if/else statement is enclosed with ___?","Arrays in JavaScript can be used to store which of the following data types?","String values must be enclosed within ___ when being assigned to variables?","A very useful tool used during development and debugging for printing content to the debugger is?"];
-
-// creates five objects that contain the four answer choices
-ansObjOne = {
-    ansOne: "Strings",
-    ansTwo: "Booleans",
-    ansThree: "Alerts",
-    ansFour: "Numbers"
-};
-
-ansObjTwo = {
-    ansOne: "Quotes",
-    ansTwo: "Curly brackets",
-    ansThree: "Parenthesis",
-    ansFour: "Square brackets"
-};
-
-ansObjThree = {
-    ansOne: "Numbers and strings",
-    ansTwo: "Other arrays",
-    ansThree: "Booleans",
-    ansFour: "All of the above",
-};
-
-ansObjFour = {
-    ansOne: "Commas",
-    ansTwo: "Curly brackets",
-    ansThree: "Quotes",
-    ansFour: "Parenthesis"
-};
-
-ansObjFive = {
-    ansOne: "JavaScript",
-    ansTwo: "Terminal/Git Bash",
-    ansThree: "For loops",
-    ansFour: "Console.log",
-};
-
-// places those objects in an array at the same index as their corresponding question
-var answersArray = [ansObjOne, ansObjTwo, ansObjThree, ansObjFour, ansObjFive];
-
-// // creates an array with the correct string answer
-// var correctArray = ["Alerts","Parenthesis","All of the above","Quotes","Console.log"];
 
 var startQuiz = function() {
     // calls the timer to start counting down (keeping score)
@@ -91,49 +50,95 @@ var startQuiz = function() {
 
 // function changes the text to iterate through the questions/answers and styles accordingly
 var createQuiz = function() {
-    quizQuestionEl.textContent = questionArray[arrayIndex];
-    listItemOneEl.textContent = answersArray[arrayIndex].ansOne;
-    listItemTwoEl.textContent = answersArray[arrayIndex].ansTwo;
-    listItemThreeEl.textContent = answersArray[arrayIndex].ansThree;
-    listItemFourEl.textContent = answersArray[arrayIndex].ansFour;
+    // creates the array containing each question to be iterated through
+    var questionArray = ["Which of the following is NOT a commonly used data type?","The condition in an if/else statement is enclosed with ___?","Arrays in JavaScript can be used to store which of the following data types?","String values must be enclosed within ___ when being assigned to variables?","A very useful tool used during development and debugging for printing content to the debugger is?"];
 
-    listItemOneEl.className = "answers";
-    listItemTwoEl.className = "answers";
-    listItemThreeEl.className = "answers";
-    listItemFourEl.className = "answers";
+    // creates five objects that contain the four answer choices
+    ansObjOne = {
+        ansOne: "Strings",
+        ansTwo: "Booleans",
+        ansThree: "Alerts",
+        ansFour: "Numbers"
+    };
 
-    listItemOneEl.addEventListener("mouseover",function(event) {
-        listItemOneEl.className = "answers-hover";
-    });
+    ansObjTwo = {
+        ansOne: "Quotes",
+        ansTwo: "Curly brackets",
+        ansThree: "Parenthesis",
+        ansFour: "Square brackets"
+    };
 
-    listItemOneEl.addEventListener("mouseout",function(event) {
+    ansObjThree = {
+        ansOne: "Numbers and strings",
+        ansTwo: "Other arrays",
+        ansThree: "Booleans",
+        ansFour: "All of the above",
+    };
+
+    ansObjFour = {
+        ansOne: "Commas",
+        ansTwo: "Curly brackets",
+        ansThree: "Quotes",
+        ansFour: "Parenthesis"
+    };
+
+    ansObjFive = {
+        ansOne: "JavaScript",
+        ansTwo: "Terminal/Git Bash",
+        ansThree: "For loops",
+        ansFour: "Console.log",
+    };
+
+    // places those objects in an array at the same index as their corresponding question
+    var answersArray = [ansObjOne, ansObjTwo, ansObjThree, ansObjFour, ansObjFive];
+
+    if (arrayIndex < questionArray.length) {
+
+        quizQuestionEl.textContent = questionArray[arrayIndex];
+        listItemOneEl.textContent = answersArray[arrayIndex].ansOne;
+        listItemTwoEl.textContent = answersArray[arrayIndex].ansTwo;
+        listItemThreeEl.textContent = answersArray[arrayIndex].ansThree;
+        listItemFourEl.textContent = answersArray[arrayIndex].ansFour;
+    
         listItemOneEl.className = "answers";
-    });
-
-    listItemTwoEl.addEventListener("mouseover",function(event) {
-        listItemTwoEl.className = "answers-hover";
-    });
-
-    listItemTwoEl.addEventListener("mouseout",function(event) {
         listItemTwoEl.className = "answers";
-    });
-
-    listItemThreeEl.addEventListener("mouseover",function(event) {
-        listItemThreeEl.className = "answers-hover";
-    });
-
-    listItemThreeEl.addEventListener("mouseout",function(event) {
         listItemThreeEl.className = "answers";
-    });
-
-    listItemFourEl.addEventListener("mouseover",function(event) {
-        listItemFourEl.className = "answers-hover";
-    });
-
-    listItemFourEl.addEventListener("mouseout",function(event) {
         listItemFourEl.className = "answers";
-    });
-
+    
+        listItemOneEl.addEventListener("mouseover",function(event) {
+            listItemOneEl.className = "answers-hover";
+        });
+    
+        listItemOneEl.addEventListener("mouseout",function(event) {
+            listItemOneEl.className = "answers";
+        });
+    
+        listItemTwoEl.addEventListener("mouseover",function(event) {
+            listItemTwoEl.className = "answers-hover";
+        });
+    
+        listItemTwoEl.addEventListener("mouseout",function(event) {
+            listItemTwoEl.className = "answers";
+        });
+    
+        listItemThreeEl.addEventListener("mouseover",function(event) {
+            listItemThreeEl.className = "answers-hover";
+        });
+    
+        listItemThreeEl.addEventListener("mouseout",function(event) {
+            listItemThreeEl.className = "answers";
+        });
+    
+        listItemFourEl.addEventListener("mouseover",function(event) {
+            listItemFourEl.className = "answers-hover";
+        });
+    
+        listItemFourEl.addEventListener("mouseout",function(event) {
+            listItemFourEl.className = "answers";
+        });
+    } else {
+        endQuiz();
+    };
 };
 
 var updateTimer = function(event) {
@@ -147,8 +152,13 @@ var updateTimer = function(event) {
     } else {
         timeLeft -= 10;
     }
+
     arrayIndex++;
     return createQuiz();
+};
+
+var endQuiz = function() {
+    alert('You have won!');
 };
 
 // creates the function that will house the score (timer) and count down
